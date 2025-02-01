6023532eec6c8a5bb1b76444af2ec669d68b6a73
Marek Smolana <msmolana@gmail.com>
2025-02-01 08:22:06 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
     pass
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    def find_and_assign_spouse(person: Person, spouse_type: str) -> bool:
+        if spouse_type in man and man[spouse_type] != None:
+            person = Person.people[man["name"]]
+            setattr(person, spouse_type, Person.people[man[spouse_type]])
+            return True
+        return False
+
+    for man in people:        
+        person = Person(man["name"], man["age"])
+        Person.people[person.name] = person
+    for man in people:
+        if not find_and_assign_spouse(man, "wife"):
+            find_and_assign_spouse(man, "husband")
+    return list(Person.people.values())
